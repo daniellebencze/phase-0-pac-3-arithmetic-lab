6b7840669a970e18bf5d9033908207d3d4d898ff
Danielle Bencze <daniellebencze@live.com>
2021-12-19 16:17:12 -0800

    commit tree 3f6224b5420a9a2a1ac35801f3caff1e280a59dd

--- index.js
+++ index.js
@@ -1,24 +1,63 @@
+// function add(a, b) {
+//     return a + b;
+// }
+// function subtract(a, b) {
+//     return a - b;
+// }
+// function multiply(a, b) {
+//     return a * b;
+// }
+// function divide(a, b) {
+//     return a / b;
+// }
+// function increment(a) {
+//     return a + 1;
+// }
+// function decrement(a) {
+//     return a - 1;
+// }
+// function makeInt(n) {
+//     return parseInt(n, 10)
+// }
+// function preserveDecimal(n) {
+//     return parseFloat(n)
+// }
+
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
+add(1, 1);
+
 function subtract(a, b) {
-    return a - b;
-} 
+  return a - b;
+}
+subtract(3, 2);
+
 function multiply(a, b) {
-    return a * b;
-} 
+  return a * b;
+}
+multiply(2, 2);
+
 function divide(a, b) {
-    return a / b;
+  return a / b;
 }
-function increment(a) {
-    return a + 1;
+divide(6, 2);
+
+function increment(n) {
+  return n + 1;
 }
-function decrement(a) {
-    return a - 1;
+n(4);
+
+function decrement(n) {
+  return n - 1;
 }
+n(6);
+
 function makeInt(n) {
-    return parseInt(n, 10)
+  return parseInt(n, 10);
 }
+makeInt;
+
 function preserveDecimal(n) {
-    return parseFloat(n)
+  return parseFloat(n);
 }
